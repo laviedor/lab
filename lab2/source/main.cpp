a3f51da60740422d26dd21230ed37ab51227e448
kim dong seon <barukhanlich@naver.com>
2025-09-15 14:08:06 +0900

    lab2

---
+++ lab2/source/main.cpp
@@ -0,0 +1,21 @@
+#define GLM_ENABLE_EXPERIMENTAL 
+#include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+int main()
+{
+	const double DEGREES_TO_RADIANS = glm::pi<float>() / 180.0;
+	const double RADIANS_TO_DEGREES = 180.0 / glm::pi<float>();
+
+	glm::vec3 v(2.0f, -1.0f, 1.0f);
+	glm::vec3 u(1.0f, 1.0f, 2.0f);
+	
+	float d = glm::dot(v, u);
+	float c = d / (glm::length(u) * glm::length(v));
+	float deg = glm::acos(c) * RADIANS_TO_DEGREES;
+
+	std::cout << "v 와 u의 각도: "<<deg << std::endl;
+
+	return 0;
+}
